8570cb4fd8828f32b8ab54ee499f1a47ec8022f2
Eagle-Assassin <anoop1.gk@gmail.com>
2025-08-07 10:35:44 +0530

    unet updated

--- U-Net/Dl_assignment_contractive.py
+++ U-Net/Dl_assignment_contractive.py
@@ -324,6 +324,11 @@ print('The model is trained and the one smaple output is displayed.')
 print('Question 1:\n Plot the t-SNE (use inbuilt function) on the embeddings obtained using the respective auto-encoders.\nColor the clusters using the respective ground-truth class labels.')
 time.sleep(2)
 
+#Using test Data set
+train_dataset=datasets.MNIST(root='data',
+                             train=False,
+                             download=True,
+                             transform=transform)
 
 # Function to plot t-SNE
 def plot_tsne(model):
